6a996edbed2b183310739254ddfbce72e8da2141
Gustav Svensson <gustav.svensson99@hotmail.com>
2022-05-26 11:22:19 +0200

    add new hamsterwars-backend

--- routes/hamsters.js
+++ routes/hamsters.js
@@ -1,65 +1,115 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+const router = express.Router();
 
-// Data hämtas från firestore!
-import { collection, getDocs, addDoc } from "firebase/firestore"
-import { db } from '../database/firebase.js'
+import {
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "../database/firebase.js";
+const colRef = collection(db, "hamsters");
 
+//GET ALL
+router.get("/", async (req, res) => {
+  let hamsters = [];
 
+  const snapshot = await getDocs(colRef);
+  snapshot.docs.forEach((docSnapshot) => {
+    hamsters.push({ ...docSnapshot.data(), id: docSnapshot.id });
+  });
 
-// Routes
-// RESTful == har GET, POST, PUT och DELETE
-router.get('/', async (req, res) => {
-    const colRef = collection(db, "hamsters");
-    let hamsters = [];
+  res.send(hamsters);
+});
 
-    const snapshot = await getDocs(colRef)
-    snapshot.docs.forEach((docSnapshot) => {
-	    hamsters.push({ ...docSnapshot.data(), id: docSnapshot.id });
-    });
-    console.log(hamsters);
+//GET RANDOM
+router.get("/random", async (req, res) => {
+  let hamsters = [];
 
-    res.send( hamsters )
-})
+  const snapshot = await getDocs(colRef);
+  snapshot.docs.forEach((docSnapshot) => {
+    hamsters.push({ ...docSnapshot.data(), id: docSnapshot.id });
+  });
 
+  res.send(hamsters[Math.floor(Math.random() * hamsters.length)]);
+});
 
-
+//POST
 router.post('/', async (req, res) => {
-
-    console.log('POST /hamsters/  ', req.body)
-
-    let newHamster =  {
-        name: req.body.name,
-        age: req.body.age,
-        favFood: req.body.favFood,
-        loves: req.body.loves,
-        imgName: req.body.imgName,
-        wins: req.body.wins,
-        defeats: req.body.defeats,
-        games: req.body.games
+    let newObject = req.body
+  
+    const newHamster = {
+      name: req.body.name,
+      age: req.body.age,
+      favFood: req.body.favFood,
+      loves: req.body.loves,
+      imgName: req.body.imgName,
+      wins: req.body.wins,
+      defeats: req.body.defeats,
+      games: req.body.games
     }
-
-    for (const key in newHamster) {
-
-        if(newHamster[key] === undefined) {
-            res.sendStatus(400)  //bad request
-            return
-        }
+  
+    if (Object.keys(newObject).length === 0) {
+      res.sendStatus(400)
+      return
     }
-
-    const colRef = collection(db, 'hamsters')
-
-
-    // lägg till nya objektet som ett doument i en collection
-    // AddDoc returnerar promise därför await
+  
     const newDocRef = await addDoc(colRef, newHamster)
+    res.status(200).send({ newHamster, id: newDocRef.id })
+  })
 
-    console.log('Lade till en ny hamster i ett nytt dokument med id = ', newDocRef.id);
+//GET ID
+router.get('/:id', async (req, res) => {
+    let id = req.params.id
+    // Får referensen till det specifika id som man skriver in i sökvägen
+    const docRef = doc(colRef, id)
+    // Get-funktionen är asynkron. Den hämtar datan från databasen och lägger inuti snapshot-objektet
+    const snapshot = await getDoc(docRef)
+  
+    // Hämta datan - data är ett vanligt JavaScript-objekt
+    const data = snapshot.data()
+  
+    if (snapshot.exists()) {
+      console.log(data)
+      res.status(200).send(data)
+      return
+    } else res.sendStatus(404)
+  })
 
-    res.sendStatus(200)  // POST behöver inte skicka tillbaka någon data
+//DELETE
+router.delete("/:id", async (req, res) => {
+  let idToRemove = req.params.id;
+  const docRef = doc(colRef, idToRemove);
 
-})
+  if (idToRemove !== "id-does-not-exist") {
+    await deleteDoc(docRef);
+    res.sendStatus(200);
+    return;
+  }
+  res.sendStatus(404);
+});
 
+//PUT
+router.put('/:id', async (req, res) => {
+    let oldDocId = req.params.id
+    let newData = req.body
+    const oldDocRef = doc(colRef, oldDocId)
+  
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+      console.log('Object does not exist')
+      res.sendStatus(400)
+      return
+    }
+  
+    if (oldDocId === 'id-does-not-exist') {
+      res.sendStatus(404)
+      return
+    }
+    await updateDoc(oldDocRef, newData)
+    res.sendStatus(200)
+  })
 
-
-export default router
+export default router;
--- server.js
+++ server.js
@@ -3,49 +3,45 @@
 import express from 'express'
 import cors from 'cors'
 import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const app = express()
 const PORT = process.env.PORT || 1177
 import hamsters from './routes/hamsters.js'
-import randomHamster from './routes/randomHamster.js'
-import specificHamster from './routes/specificHamster.js'
-import updateHamster from './routes/updateHamster.js'
-import deleteHamster from './routes/deleteHamster.js'
-import matches from './routes/matches.js'
-import specificMatch from './routes/specificMatch.js'
-import deleteMatches from './routes/deleteMatches.js'
-// const staticFolder = path.join(__dirname, 'public')
+// import matches from './routes/matches.js'
+// import matchWinners from './routes/matchWinners.js'
+// import winners from './routes/winners.js'
+// import losers from './routes/losers.js'
 
+const staticFolder = path.join(__dirname, '/../dist/')
+
+const staticImages = path.join(__dirname, '/public/img/')
 
 // Middleware
 // CORS öppnar vårt projekt så det kan användas från andra domäner
-app.use( cors() )
-
+app.use(cors())
+app.use(express.json())
 // Parse request body
-app.use( express.urlencoded({ extended: true }) )
+app.use(express.urlencoded({ extended: true }))
 
 // Logger - skriv ut information om inkommande request
-app.use( (req, res, next) => {
-	console.log(`Logger: ${req.method}  ${req.url} `, req.body)
-	next()
+app.use((req, res, next) => {
+  console.log(`Logger: ${req.method}  ${req.url} `, req.body)
+  next()
 })
 
 // Serve static files in this folder
-app.use( express.static('public'))
-
-
+app.use(express.static(staticFolder))
+app.use('/img', express.static(staticImages))
 
 // Routes
 app.use('/hamsters', hamsters)
-app.use('/hamsters/random', randomHamster)
-app.use('/hamsters', specificHamster)
-app.use('/hamsters', updateHamster)
-app.use('/hamsters', deleteHamster)
-app.use('/matches', matches)
-app.use('/matches', specificMatch)
-app.use('/matches', deleteMatches)
-
+app.all('*', (req, res) => {
+  res.sendFile(staticFolder + 'index.html')
+})
 
 // Starta server
 app.listen(PORT, () => {
-	console.log(`Server is listening on port ${PORT}.`)
+  console.log(`Server is listening on port ${PORT}.`)
 })
